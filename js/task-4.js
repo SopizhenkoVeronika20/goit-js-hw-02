c4631ecb29f5a5e6a8808cd57746ee83362f5524
Veronika <sopizhenkoveronika2004@gmail.com>
2024-06-20 12:12:33 +0300

    fix task-4

--- js/task-4.js
+++ js/task-4.js
@@ -2,12 +2,12 @@
 
 function getShippingCost(country) {
   switch (country.toUpperCase()) {
-    case "AUSTRALIA":
-      return `Shipping to ${country} will cost 170 credits`;
     case "CHINA":
       return `Shipping to ${country} will cost 100 credits`;
     case "CHILE":
       return `Shipping to ${country} will cost 250 credits`;
+    case "AUSTRALIA":
+      return `Shipping to ${country} will cost 170 credits`;
     case "JAMAICA":
       return `Shipping to ${country} will cost 120 credits`;
     default:
